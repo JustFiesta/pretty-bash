9ad7f5d8e55b0d9228596c443bd83ee44e9337a0
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-05-19 13:07:28 +0200

    Update setup.sh - added echoes to space out debug

--- setup.sh
+++ setup.sh
@@ -10,10 +10,12 @@ mkdir /tmp/Pretty-Bash-tmp/ && cd /tmp/Pretty-Bash-tmp/
 # Download needed config files
 
 echo ""
+echo "---------------------------------------------"
 echo "Using wget to download .bashrc arhive..."
 wget https://github.com/JustFiesta/Pretty-Bash/archive/main.zip
 
 if [ $? -eq 1 ]; then
+	echo ""
 	echo "Could not wget .bashrc arhive from GitHub."
 	exit 1
 fi
@@ -23,6 +25,7 @@ echo "Unpacking zip file..."
 unzip main.zip
 
 if [ $? -eq 1 ]; then
+	echo ""
 	echo "Could not unzip main.zip arhive."
 	exit 1
 fi
@@ -32,12 +35,14 @@ echo "Installing new .bashrc..."
 cd Pretty-Bash-main/ && mv .bashrc ~/.bashrc && cd ..
 
 if [ $? -eq 1 ]; then
+	echo ""
 	echo "Could not move new .bashrc file into HOME directory."
 	exit 1
 fi
 
 echo ""
 echo "File succesfully installed! ($HOME/.bashrc)"
+echo ""
 
 echo "---------------------------------------------"
 echo "Using wget to download .vimrc zip arhive..."
@@ -53,6 +58,7 @@ echo "Unpacking zip file..."
 unzip master.zip
 
 if [ $? -eq 1 ]; then
+	echo ""
 	echo "Could not unzip main.zip arhive."
 	exit 1
 fi
@@ -62,6 +68,7 @@ echo "Installing new .bashrc..."
 cd Basic-vim-master/ && mv .vimrc ~/.vimrc
 
 if [ $? -eq 1 ]; then
+	echo ""
 	echo "Could not move new .vimrc file into HOME directory."
 	exit 1
 fi
@@ -74,31 +81,36 @@ echo "Removing tmp/Pretty-Bash-tmp/"
 rm -r /tmp/Pretty-Bash-tmp/
 
 if [ $? -eq 1 ]; then
+	echo ""
 	echo "Could not remove tmp folder. Please proceed manually."
 	exit 1
 fi
 
 # Download VIM plugin manager
 
+echo ""
 echo "---------------------------------------------"
+
 # Create folders
 echo "Creating .vim folder..."
 mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
 
 if [ $? -eq 1 ]; then
+	echo ""
         echo "Could not create .vim folder."
         exit 1
 fi
 
 
 # Download vim-plug
-
+echo ""
 echo "---------------------------------------------"
 echo "Using curl to download vim-plug (plugin manager)..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 if [ $? -eq 1 ]; then
+	echo ""
         echo "Could not download vim-plug plugin manager."
         exit 1
 fi
@@ -111,12 +123,17 @@ source $HOME/.vimrc
 
 
 # Install VIM plugins
+
+echo ""
 echo "---------------------------------------------"
 echo "Installing VIM plugins..."
 vim +PlugInstall +qall
 
 if [ $? -eq 1 ]; then
+	echo ""
 	echo "Error during plugin installation. Please install them manuanlly in vim using :PlugInstall"
         exit 1
 fi
+
+echo ""
 echo "VIM plugins installed!"
